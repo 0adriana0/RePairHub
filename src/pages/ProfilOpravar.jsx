f43a317c4b309233b9af0127ad0246e6bd1a515a
Wisla-15 <ondra.w08@gmail.com>
2025-11-02 21:39:05 +0100

    Změna profilovky - ještě není hotovo

--- src/pages/ProfilOpravar.jsx
+++ src/pages/ProfilOpravar.jsx
@@ -7,14 +7,17 @@ import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
 import Button from '../components/Button'
 import ChangeRoleBtn from '../components/ChangeRoleBtn'
-
+import pen from '../img/pen.png'
+import axios from 'axios'
+ 
 const ProfilOpravar = () => {
+
+  
   const navigate = useNavigate()
 
   // Informace z databáze
-  const [pfp, setPfp] = useState(defaultPfp)
   const [name, setName] = useState('Načítám data...')
-  const [lastName, setLastName] = useState('')
+  const [lastName, setLastName] = useState('Načítám data...')
   const [email, setEmail] = useState('Načítám data...')
   const [location, setLocation] = useState('Načítám data...')
   const [bio, setBio] = useState('Načítám data...')
@@ -26,6 +29,44 @@ const ProfilOpravar = () => {
   const [locationClass, setLocationClass]= useState('informace')
   const [emailClass, setEmailClass]= useState('informace')
   const [bioClass, setBioClass]= useState('informace')
+
+  // Změna pfp
+  const [pfp, setPfp] = useState(defaultPfp)
+  const [changingPfp, setChangingPfp] = useState(false)
+  const [pfpUrl, setPfpUrl] = useState('')
+  const [pfpPublicId, setPfpPublicId] = useState('')
+
+  const handleChangePfp = async (e)=>{
+    const file = (e.target.files[0])
+    if(!file) return;
+    setPfp(file)
+    const user = auth.currentUser
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('upload_preset', 'RePairHub')
+    formData.append('folder', `users/${user.uid}`)
+    
+    try{
+      const resp = await axios.post('https://api.cloudinary.com/v1_1/dmisabll4/image/upload', formData)
+      setPfpUrl(resp.data.secure_url)
+      setPfp(resp.data.secure_url)
+      setPfpPublicId(resp.data.public_id)
+    } catch(err){alert(err.message)}
+    setChangingPfp(false)
+  }
+
+  const PfpSet = ({onChange}) => {
+  return <div className='pfp-set'>
+      <p>Zvolte obrázek</p>
+      <input 
+        type="file"
+        onChange={onChange}
+      />
+      <p style={{color: '#009B73', marginBottom: '40px'}}>Změna se projeví při příštím přihlášení</p>
+      <Button onClick={()=>setChangingPfp(false)}>Zrušit</Button>
+    </div>
+  
+}
 
 
   // Změny na profilu
@@ -55,13 +96,14 @@ const ProfilOpravar = () => {
     setBioClass('changed-info')
   }
 
+ 
 
   // Uložení nových dat
   const saveChanges = async ()=>{
     try {
       const user = auth.currentUser
       const userRef = doc(db, 'users', user.uid)
-      await updateDoc(userRef, {name, lastName, location, email, bio})  
+      await updateDoc(userRef, {name, lastName, location, email, pfp, bio, pfp: pfpUrl, pfpPublicId})  
     } catch(err) {alert(err.message)}
     setNameClass('informace')
     setLastNameClass('informace')
@@ -79,12 +121,13 @@ const ProfilOpravar = () => {
         const user = auth.currentUser
         const userRef = doc(db, 'users', user.uid)
         const userData = (await getDoc(userRef)).data()
-        const {name, lastName, email, location, bio } = userData
+        const {name, lastName, email, location, bio, pfp} = userData
         setName(name)
         setLastName(lastName)
         setEmail(email)
         setLocation(location)
-        setBio(bio)
+        setBio(bio || '')
+        setPfp(pfp || defaultPfp)
       }
     try{
       loadData()
@@ -92,11 +135,16 @@ const ProfilOpravar = () => {
   }, [navigate])
 
 
-
   return (<div className='profil-opravar-all'>
     <ProfilHeader nadpisText='Upravit profil' />
-      <div className="profil-opravar">
-          <img src={pfp} alt="" />
+      <div className={changingPfp ? 'profil-opravar-smaller profil-opravar' :"profil-opravar"}>
+
+
+          <img src={pfp} alt={defaultPfp} className='pfp'/>
+          <img src={pen} alt='' className='pen' onClick={()=>setChangingPfp(!changingPfp)}/>
+          {changingPfp && <PfpSet onChange={(e)=>handleChangePfp(e)}/>}
+
+        { !changingPfp &&
           <form onSubmit={(e)=>e.preventDefault()}>
             <p id='top-profile'>Jméno</p>
             <input 
@@ -131,12 +179,13 @@ const ProfilOpravar = () => {
               type="text"
               value={bio}
               placeholder='Napište něco o sobě'
-              onChange={(e)=>changeBio}
+              onChange={(e)=>changeBio(e)}
               className={bioClass}
             />
             <ChangeRoleBtn />
             <Button onClick={()=>saveChanges()}>Uložit změny</Button>
           </form>
+        }
       </div>
     </div>
   )
